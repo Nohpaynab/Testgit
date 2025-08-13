e509bd221e00d66302ba527477bbd549beeb43b4
Banyaphon.wpk <Banyaphon.wpk@gmail.com>
2025-08-13 17:20:32 +0700

    Revert "add link to contact page"
    This reverts commit 48c72ec815d7be383aa16c58712c96384bf7dd8d.

--- index.php
+++ index.php
@@ -12,10 +12,16 @@
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	</p>
+<<<<<<< HEAD
 	<a href="contact.php">go to Page Contact</a>
 	<a href="about.php">go to Page About</a>
 	<a href="Page1.php">go to Page 1</a>
 
+=======
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+>>>>>>> parent of 48c72ec (add link to contact page)
 </div>
 
 </body>
